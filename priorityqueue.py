d08962e99e43740e0a09852020da256ea2573beb
SU Zhiyang <suzhiyang@gmail.com>
2014-04-03 13:39:42 +0800

    Weighted set cover

---
+++ priorityqueue.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# Copyright (C) 2014 Zhiyang Su
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import itertools
+from heapq import *
+
+class PriorityQueue:
+    def __init__(self):
+        self._pq = []
+        self._entry_map = {}
+        self._counter = itertools.count()
+
+    def addtask(self, task, priority = 0):
+        '''Add a new task or update the priority of an existing task'''
+        if task in self._entry_map:
+            self.removetask(task)
+        count = next(self._counter)
+        entry = [priority, count, task]
+        self._entry_map[task] = entry
+        heappush(self._pq, entry)
+
+    def removetask(self, task):
+        '''Mark an existing task as REMOVED.'''
+        entry = self._entry_map.pop(task)
+        entry[-1] = 'removed'
+
+    def poptask(self):
+        '''Remove and return the lowest priority task.'''
+        while self._pq:
+            priority, count, task = heappop(self._pq)
+            if task is not 'removed':
+                del self._entry_map[task]
+                return task
+
+    def __len__(self):
+        return len(self._entry_map)
+
+if __name__ == "__main__":
+    pq = PriorityQueue()
+    pq.addtask(1, 3)
+    pq.addtask(2, 4)
+    pq.addtask(3, 5)
+    pq.addtask(2, 1)
+    pq.addtask(3, 0.5)
+
+    while pq:
+        print pq.poptask()
